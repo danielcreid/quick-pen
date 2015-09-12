a7e2435abe006059d30fb12c83806e90fc0a10f9
Daniel Reid <danielc.reid@gmail.com>
2015-09-12 17:29:45 -0500

    Single preset that mostly works

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,26 +1,138 @@
-// TODO:
-// - Use data-binds where appropriate
+// Helper functions
+// -----------------------------------------------------------------------------
+
+function objectIsEmpty(object) {
+    return Object.keys(object).length === 0;
+};
+
+
+
+// Prep data for CodePen form.submit
+
+function stringReplace(obj) {
+    var string = JSON.stringify(obj);
+    string = string.replace(/"/g, "&quot;");
+    string = string.replace(/'/g, "&apos;");
+    return string;
+};
+
+
+
+// Chrome extension helpers
+
+function saveData(data, callback) {
+    chrome.storage.sync.set(data, callback);
+};
+
+function getData(callback) {
+    chrome.storage.sync.get(null, callback);
+};
+
+function clearData(callback) {
+    chrome.storage.sync.clear(callback);
+};
+
+
+
+
+
+// Data management
+// -----------------------------------------------------------------------------
+
+var defaultData = {
+    config: {
+        html_pre_processors: ['slim', 'haml', 'markdown'],
+        css_pre_processors: ['less', 'scss', 'sass', 'stylus'],
+        css_starters: ['normalize', 'reset'],
+        css_prefixes: ['autoprefixer', 'prefixfree'],
+        js_pre_processors: ['coffeescript', 'babel', 'livescript', 'typescript']
+    },
+    presets: [{
+        id: 1,
+        name: 'Default',
+        title: '',
+        description: '',
+        tags: null,
+        editors_html: true,
+        editors_css: true,
+        editors_js: true,
+        html: '',
+        html_pre_processor: '',
+        css: '',
+        css_pre_processor: '',
+        css_starter: '',
+        css_prefix: '',
+        js: '',
+        js_pre_processor: '',
+        html_classes: '',
+        head: '<meta name="viewport" content="width=device-width, initial-scale=1">',
+        css_external: '',
+        js_external: ''
+    }]
+};
+
+// Runs on install and reload
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.get(null, function(data) {
+        if (objectIsEmpty(data)) {
+            saveData(defaultData);
+        }
+    });
+});
+
+
+
+
+
+// Browser action
+// -----------------------------------------------------------------------------
+
+
+
+
+
+// Background Page
+// -----------------------------------------------------------------------------
 
 function background() {
     var codepenForm = document.querySelector('.js-codepen-form');
-    var codepenFormInput = codepenForm.firstElementChild;
+    var codepenFormData = ko.observable('');
 
 
+    function transformEditors(data) {
+        var array = [];
 
-    function getData(callback) {
-        chrome.storage.sync.get(null, callback);
-    };
+        array.push(data.editors_html | 0);
+        array.push(data.editors_css | 0);
+        array.push(data.editors_js | 0);
 
-    function stringReplace(obj){
-        var string = JSON.stringify(obj);
-        string = string.replace(/"/g, "&quot;");
-        string = string.replace(/'/g, "&apos;");
+        var string = array.join('');
+
         return string;
     };
 
     function createNewPen() {
-        getData(function(data){
-            codepenFormInput.value = stringReplace(data);
+        chrome.storage.sync.get(null, function(data) {
+            var codepen_submit = {
+                title: data.presets[0].title,
+                description: data.presets[0].description,
+                tags: data.presets[0].tags,
+                editors: transformEditors(data.presets[0]),
+                html: data.presets[0].html,
+                html_pre_processor: data.presets[0].html_pre_processor,
+                css: data.presets[0].css,
+                css_pre_processor: data.presets[0].css_pre_processor,
+                css_starter: data.presets[0].css_starter,
+                css_prefix: data.presets[0].css_prefix,
+                js: data.presets[0].js,
+                js_pre_processor: data.presets[0].js_pre_processor,
+                html_classes: data.presets[0].html_classes,
+                head: data.presets[0].head,
+                css_external: data.presets[0].css_external,
+                js_external: data.presets[0].js_external
+            };
+
+            codepenFormData(stringReplace(codepen_submit));
             codepenForm.submit();
         });
     };
@@ -31,7 +143,7 @@ function background() {
 
     var vm = {
         codepenForm: codepenForm,
-        codepenFormInput: codepenFormInput
+        codepenFormData: codepenFormData
     };
 
     return vm;
--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,76 +1,63 @@
+////////////////////////////////////////////////////////////////////////////////
 // TODO:
-// - Initialize saved data and populate fields
 // - Allow for multiple values for appropriate fields (external assets, tags)
+// - Tags doesn't work
 // - Trim values if/when necessary
 // - Add friendly display names for <select> options
-// - Figure out what to do with the "private" option
+// - Add ability to have multiple presets
+////////////////////////////////////////////////////////////////////////////////
+
+// Helper functions
+// -----------------------------------------------------------------------------
+
+// Chrome extension helpers
+
+function saveData(data, callback) {
+    chrome.storage.sync.set(data, callback);
+};
+
+function getData(callback) {
+    chrome.storage.sync.get(null, callback);
+};
+
+function clearData(callback) {
+    chrome.storage.sync.clear(callback);
+};
+
+
+
+
+
+// Data management
+// -----------------------------------------------------------------------------
 
 function options() {
-    var title = ko.observable('');
-    var description = ko.observable('');
-    var tags = ko.observableArray([]);
-    //var private = ko.observable(false);
-
-    var editorsHTML = ko.observable(true);
-    var editorsCSS = ko.observable(true);
-    var editorsJS = ko.observable(true);
-
-    var html = ko.observable('');
-
-    var HTMLPreprocessors = ko.observableArray(['slim', 'haml', 'markdown']);
-    var selectedHTMLPreprocessor = ko.observable();
-
-    var css = ko.observable(''); // "html { color: red; }",
-
-    var CSSPreprocessors = ko.observableArray(['less', 'scss', 'sass', 'stylus']);
-    var selectedCSSPreprocessor = ko.observable();
-
-    var CSSBases = ko.observableArray(['normalize', 'reset']);
-    var selectedCSSBase = ko.observable();
-
-    var vendorPrefixes = ko.observableArray(['autoprefixer', 'prefixfree']);
-    var selectedVendorPrefix = ko.observable();
-
-    var js = ko.observable('');
-
-    var JSPreprocessors = ko.observableArray(['coffeescript', 'babel', 'livescript', 'typescript']);
-    var selectedJSPreprocessor = ko.observable();
-
-    var HTMLClasses = ko.observable('');
-    var head = ko.observable('<meta name="viewport" content="width=device-width, initial-scale=1">');
-    var externalCSS = ko.observable('');
-    var externalJS = ko.observable('')
-
-
-
-    var selectedEditors = ko.computed(function() {
-        var array = [];
-        var string;
-
-        array.push(editorsHTML() | 0);
-        array.push(editorsCSS() | 0);
-        array.push(editorsJS() | 0);
-
-        string = array.join('');
-
-        return string;
-    });
-
-
-
-    // Chrome extension helpers
-
-    function saveData(data, callback) {
-        chrome.storage.sync.set(data, callback);
-    };
-
-    function getData(callback) {
-        chrome.storage.sync.get(null, callback);
-    };
-
-    function clearData(callback) {
-        chrome.storage.sync.clear(callback);
-    };
+    //var presets = ko.observableArray();
+    //var preset = ko.observable();
+    var title = ko.observable();
+    var description = ko.observable();
+    var tags = ko.observableArray();
+    var editors_html = ko.observable();
+    var editors_css = ko.observable();
+    var editors_js = ko.observable();
+    var editors = ko.observable(); // Computed
+    var html = ko.observable();
+    var html_pre_processors = ko.observableArray();
+    var html_pre_processor = ko.observable();
+    var css = ko.observable();
+    var css_pre_processors = ko.observableArray();
+    var css_pre_processor = ko.observable();
+    var css_starters = ko.observableArray();
+    var css_starter = ko.observable();
+    var css_prefixes = ko.observableArray();
+    var css_prefix = ko.observable();
+    var js = ko.observable();
+    var js_pre_processors = ko.observableArray();
+    var js_pre_processor = ko.observable();
+    var html_classes = ko.observable();
+    var head = ko.observable();
+    var css_external = ko.observable(); // Will be an array
+    var js_external = ko.observable(); // Will be an array
 
 
 
@@ -91,74 +78,119 @@ function options() {
 
 
 
-    var saveForm = function() {
-        // Data gets formatted according to CodePen's JSON data format
-        var data = {
-            title : title(),
-            description : description(),
-            //private : private(),
-            tags : tags(),
-            editors : selectedEditors(),
-            html : html(),
-            html_pre_processor : selectedHTMLPreprocessor(),
-            css : css(),
-            css_pre_processor : selectedCSSPreprocessor(),
-            css_starter : selectedCSSBase(),
-            css_prefix : selectedVendorPrefix(),
-            js : js(),
-            js_pre_processor : selectedJSPreprocessor(),
-            html_classes : HTMLClasses(),
-            head : head(),
-            css_external : externalCSS(),
-            js_external : externalJS()
+    // If you change the preset dropdown, load a new preset
+    //preset.subscribe(loadPreset, this);
+
+    function loadPreset() {
+        chrome.storage.sync.get(null, function(data) {
+            //data.presets.forEach(function(obj) {
+                //if (obj.id === preset) {
+                    title(data.presets[0].title);
+                    description(data.presets[0].description);
+                    tags(data.presets[0].tags);
+                    editors_html(data.presets[0].editors_html);
+                    editors_css(data.presets[0].editors_css);
+                    editors_js(data.presets[0].editors_js);
+                    html(data.presets[0].html);
+                    html_pre_processor(data.presets[0].html_pre_processor);
+                    css(data.presets[0].css);
+                    css_pre_processor(data.presets[0].css_pre_processor);
+                    css_starter(data.presets[0].css_starter);
+                    css_prefix(data.presets[0].css_prefix);
+                    js(data.presets[0].js);
+                    js_pre_processor(data.presets[0].js_pre_processor);
+                    html_classes(data.presets[0].html_classes);
+                    head(data.presets[0].head);
+                    css_external(data.presets[0].css_external);
+                    js_external(data.presets[0].js_external);
+                //}
+            //});
+        });
+    };
+
+    var savePreset = function() {
+        var preset = {
+            presets: [{
+                title: title(),
+                description: description(),
+                tags: tags(),
+                editors_html: editors_html(),
+                editors_css: editors_css(),
+                editors_js: editors_js(),
+                html: html(),
+                html_pre_processor: html_pre_processor(),
+                css: css(),
+                css_pre_processor: css_pre_processor(),
+                css_starter: css_starter(),
+                css_prefix: css_prefix(),
+                js: js(),
+                js_pre_processor: js_pre_processor(),
+                html_classes: html_classes(),
+                head: head(),
+                css_external: css_external(),
+                js_external: js_external()
+            }]
         };
 
-        saveData(data, logSavedData);
+        chrome.storage.sync.set(preset, logSavedData);
     };
 
 
 
+    function initializeFormOptions() {
+        chrome.storage.sync.get(null, function(data) {
+            // Basic options
+            html_pre_processors(data.config.html_pre_processors);
+            css_pre_processors(data.config.css_pre_processors);
+            js_pre_processors(data.config.js_pre_processors);
+            css_starters(data.config.css_starters);
+            css_prefixes(data.config.css_prefixes);
+
+            // Load presets
+            //data.presets.forEach(function(preset) {
+            //    presets.push(preset.id);
+            //});
+
+            // Load selected preset form data
+            loadPreset();
+        });
+    };
+
+    initializeFormOptions();
+
+
+
     var vm = {
-        title : title,
-        description : description,
-        tags : tags,
-        //private : private,
+        //presets: presets,
+        //preset: preset,
+        title: title,
+        description: description,
+        tags: tags,
+        editors_html: editors_html,
+        editors_css: editors_css,
+        editors_js: editors_js,
+        editors: editors,
+        html: html,
+        html_pre_processors: html_pre_processors,
+        html_pre_processor: html_pre_processor,
+        css: css,
+        css_pre_processors: css_pre_processors,
+        css_pre_processor: css_pre_processor,
+        css_starters: css_starters,
+        css_starter: css_starter,
+        css_prefixes: css_prefixes,
+        css_prefix: css_prefix,
+        js: js,
+        js_pre_processors: js_pre_processors,
+        js_pre_processor: js_pre_processor,
+        html_classes: html_classes,
+        head: head,
+        css_external: css_external,
+        js_external: js_external,
 
-        editorsHTML : editorsHTML,
-        editorsCSS : editorsCSS,
-        editorsJS : editorsJS,
-
-        html : html,
-
-        HTMLPreprocessors : HTMLPreprocessors,
-        selectedHTMLPreprocessor : selectedHTMLPreprocessor,
-
-        css : css,
-
-        CSSPreprocessors : CSSPreprocessors,
-        selectedCSSPreprocessor : selectedCSSPreprocessor,
-
-        CSSBases : CSSBases,
-        selectedCSSBase : selectedCSSBase,
-
-        vendorPrefixes : vendorPrefixes,
-        selectedVendorPrefix : selectedVendorPrefix,
-
-        js : js,
-
-        JSPreprocessors : JSPreprocessors,
-        selectedJSPreprocessor : selectedJSPreprocessor,
-
-        HTMLClasses : HTMLClasses,
-        head : head,
-        externalCSS : externalCSS,
-        externalJS : externalJS,
-
-        selectedEditors : selectedEditors,
-
-        logSavedData : logSavedData,
-        clearSavedData : clearSavedData,
-        saveForm : saveForm
+        logSavedData: logSavedData,
+        clearSavedData: clearSavedData,
+        savePreset: savePreset
     };
 
     return vm;
